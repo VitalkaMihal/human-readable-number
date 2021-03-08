e4c7c857e632d6cda4cfb6ce2921b1b514279e9b
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2021-03-08 16:16:40 +0300

    feat: add number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 module.exports = function toReadable (number) {
    let arr = number.toString().split('');
    let arr19 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-   let arr1019 = ['ten', 'eleven', 'twelwe', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+   let arr1019 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
    let arr2090 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-   let arr100 = ['hundred'];
-   if (number <= 9) {
+     if (number === 0){
+       return 'zero';
+   } else if (number <= 9) {
      return arr19[number];
    } else if ( 10 <= number && number <=19) {
      return arr1019[number - 10];
@@ -15,6 +16,10 @@ module.exports = function toReadable (number) {
      return arr2090[+arr[0] - 2] + ' ' + arr19[+arr[1]];
       }
    } else if ( 100 <= number && number <= 999) {
-     return arr19[+arr[0]] + ' ' + arr100 + ' ' + moduleexports(number % 100);
+       if (number % 100 === 0){
+           return arr19[+arr[0]] + ' ' + 'hundred';
+       } else {
+     return arr19[+arr[0]] + ' ' + 'hundred' + ' ' + moduleexports(number % 100);
    }
+  }
 }
